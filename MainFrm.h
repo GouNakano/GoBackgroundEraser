582f5dae1ac3170cd48df7fa775ae8c54b506301
nakano <access@sakura-densan.com>
2023-09-16 19:41:03 +0900

    202309161940

--- MainFrm.h
+++ MainFrm.h
@@ -39,14 +39,35 @@ __published:	// IDE で管理されるコンポーネント
 	void __fastcall MainPanelResize(TObject *Sender);
 	void __fastcall TimerTimer(TObject *Sender);
 private:
+	//マウスイベント
+	static void onmouse(int event,int x,int y,int flags,void *param);
+private:
 	TBGEraser BGEraser;
 	HWND      hWnd1;
 	HWND      hWnd1_parent;
 	//ウィンドウサイズ
 	cv::Size win_sz;
 private:
+	int      ix;
+	int      iy;
+	cv::Rect rect;
+	cv::Mat  disp_mat;
+	cv::Mat  disp_mask_mat;
+	cv::Mat  original_mat;
+	cv::Mat  original_mask_mat;
+private:
 	//モード表示
 	void dispMode(const std::string& mode_str);
+	//元画像の表示画像を作成する
+	void makeDrawMatFromOrignalMat();
+	//描画用Matで表示更新
+	void updateDispFromDrawMat();
+	//表示用矩形を元画像の大きさに合わせる
+	bool adjustRectFromDrawRect(const cv::Rect& draw_rect,cv::Rect& adjust_rect);
+	//表示用画像を元画像の大きさに合わせる
+	bool adjustDispMatFromDrawMat(const cv::Mat& draw_mat,cv::Mat& adjust_mat);
+	//表示用画像から元画像への座標変換
+	bool adjustOriginalPointFromDrawPoint(const cv::Point& draw_point,cv::Point& adjust_point);
 public:
 	__fastcall TMainForm(TComponent* Owner);
 };
--- TBGEraser.h
+++ TBGEraser.h
@@ -55,31 +55,31 @@ public:
 	//変換範囲の矩形の色
 	static const TmaskInf DRAW_RECTANGLE;
 private:
-	cv::Mat    img;
-	cv::Mat    img2;
-	cv::Mat    mask;
 	cv::Mat    mask2;
-	cv::Mat    output;
 	cv::Mat    bgdmodel;
 	cv::Mat    fgdmodel;
 	cv::Mat    disp_mat;
-	bool       drawing;
-	TmaskInf   value;
+public:
+	cv::Mat    mask;
+	cv::Mat    img;
+	cv::Mat    img_org;
+	cv::Mat    output;
 	bool       rectangle;
-	cv::Rect   rect;
-	int        rect_or_mask;
 	int        ix;
 	int        iy;
+	cv::Rect   rect;
+	int        rect_or_mask;
 	bool       rect_over;
+	bool       drawing;
 	int        thickness;
+	TmaskInf   value;
+
 
 private:
 	//srcの特定の値(v)のピクセルをnew_val、その他は変更しない
 	bool setNewValFromVal(const cv::Mat& src,cv::Mat& dst,int new_val,int v);
 	//mat1,mat2の同じ値の所をnew_valその他を0にする
 	bool setNewValFromSamePixcel(const cv::Mat& mat1,const cv::Mat& mat2,cv::Mat& dst,int new_val);
-	//マスクを更新して出力画像を作成する
-	bool updateMaskAndOutputImage();
 public:
 	//マウスイベント
 	static void onmouse(int event,int x,int y,int flags,void *param);
@@ -107,6 +107,15 @@ public:
 	bool setSpecifyForegroundMode();
 	//出力Matを得る
 	bool getOutputMat(cv::Mat& output_mat);
+	//出力マスクを得る
+	bool getOutputMasktMat(cv::Mat& mask_mat);
+	//元画像Matを得る
+	bool getOriginalMat(cv::Mat& original_mat);
+	//元マスクを得る
+	bool getOriginalMaskMat(cv::Mat& original_mask_mat);
+public:
+	//マスクを更新して出力画像を作成する
+	bool updateMaskAndOutputImage();
 };
 
 #endif
